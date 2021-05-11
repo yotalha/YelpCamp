95763c7005a21b31c63e3087282d81552014f230
yotalha <talha.coder9@gmail.com>
2021-05-11 14:17:01 +0500

    images added

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const Campground = mongoose.model('Campground', new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String
 }));
